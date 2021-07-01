ebc1fb331656830d906a0f54b72563f541120236
Tres <tres.sims95@gmail.com>
2021-06-30 23:46:42 -0400

    added analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -2,12 +2,23 @@ import Head from 'next/head';
 import styles from '../styles/main.scss';
 function MyApp({ Component, pageProps }) {
   return (
-    <>
-      <Head>
-        <link rel="stylesheet" href="https://use.typekit.net/lej3fwk.css" />
-      </Head>
-      <Component {...pageProps} />
-    </>
+      <>
+	  <Head>
+              <link rel="stylesheet" href="https://use.typekit.net/lej3fwk.css" />
+	      <script async src="https://www.googletagmanager.com/gtag/js?id=G-FTHGLGM87J"></script>
+	      <script
+		  async
+	      dangerouslySetInnerHTML={{
+		 __html: ` window.dataLayer = window.dataLayer || [];
+		  function gtag(){dataLayer.push(arguments);}
+		  gtag('js', new Date());
+
+		  gtag('config', 'G-FTHGLGM87J');`
+	      }}
+	      />
+	  </Head>
+	  <Component {...pageProps} />
+      </>
   );
 }
 
