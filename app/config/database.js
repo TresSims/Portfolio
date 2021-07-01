732484a74583975e509266fda0b5c4e5db7671be
Tres <tres.sims95@gmail.com>
2021-07-01 00:27:26 -0400

    configure postgres on backend

--- app/config/database.js
+++ app/config/database.js
@@ -4,8 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
+        client: env('DATABASE_CLIENT', 'postgres'),
+	host: env('DATABASE_HOST', 'db'),
+	port: env('DATABASE_PORT', 5432),
+	database: env('DATABASE_NAME', 'strapi'),
+	username: env('DATABASE_USERNAME', 'postgres'),
+	password: env('DATABASE_PASSWORD', 'postgres'),
         filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+	ssl: env('DATABASE_SSL', false),
       },
       options: {
         useNullAsDefault: true,
