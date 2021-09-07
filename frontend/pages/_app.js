22ab4a86e5a32715c5206f3ba2c96de119bdbfc3
Tres <tres.sims95@gmail.com>
2021-09-07 17:49:01 -0400

    some work

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,20 +1,26 @@
 import Head from 'next/head';
 import styles from '../styles/main.scss';
 function MyApp({ Component, pageProps }) {
-  return (
-      <>
-	  <Head>
-              <link rel="stylesheet" href="https://use.typekit.net/lej3fwk.css" />
-	      <script async src="https://www.googletagmanager.com/gtag/js?id=G-FTHGLGM87J"></script>
-	      <script
-		  async
-	      dangerouslySetInnerHTML={{
-		 __html: ` window.dataLayer = window.dataLayer || [];
+    let analyticsTag = "";
+    try{
+	analyticsTag = process.env('NEXT_PUBLIC_ANALYTICS_ID');
+    }catch{
+	console.log("no analytics tag defined!");
+    }
+    return (
+	<>
+	<Head>
+        <link rel="stylesheet" href="https://use.typekit.net/lej3fwk.css" />
+	<script async src={`https://www.googletagmanager.com/gtag/js?id=${analyticsTag}`}></script>
+	<script
+	async
+	dangerouslySetInnerHTML={{
+	    __html: ` window.dataLayer = window.dataLayer || [];
 		  function gtag(){dataLayer.push(arguments);}
 		  gtag('js', new Date());
 
-		  gtag('config', 'G-FTHGLGM87J');`
-	      }}
+		  gtag('config', '${analyticsTag}');`
+	}}
 	      />
 	  </Head>
 	  <Component {...pageProps} />
